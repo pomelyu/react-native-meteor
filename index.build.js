43f8e2fbbf27ada5eac6cc8a4f9611a22aa54dde
pomelyu <pomelyu5199@icloud.com>
2018-02-26 15:01:10 +0800

    :wrench: modifiy build script

---
+++ index.build.js
@@ -0,0 +1,11 @@
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+const Meteor = require('./dist');
+
+exports.default = Meteor;
+exports.createContainer = Meteor.createContainer;
+exports.Accounts = Meteor.Accounts;
+exports.withTracker = Meteor.withTracker;
+exports.ReactiveDict = Meteor.ReactiveDict;
